25a462f87c63499f526f98a29095833eebd714bc
Mario Quinteros <marioquinteros21@gmail.com>
2021-09-27 23:10:11 -0300

    ...

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './NavBar.css';
+
+var data = [
+  { string: 'Nosotros', id: '1' },
+  { string: 'Productos', id: '1' },
+  { string: 'Contacto', id: '1' },
+];
+
+const NavBar = () => {
+  return (
+    <div className='nav-container'>
+      <h1 className='nav-logo'>Georgias Wines</h1>
+      <ul className='nav-items'>
+        {data.map((data) => (
+          <li key={data.id}>
+            <a href='/' className='nav-item'>
+              {data.string}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
+
+
